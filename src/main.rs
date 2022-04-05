d3d632fe1a8e1da687c933f3a5a51bea952b9c45
arata <49086220+arata-nvm@users.noreply.github.com>
2022-04-05 04:44:45 +0000

    feat: basic shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    ffi,
+    io::{self, Write},
+};
+
+use nix::{sys::wait, unistd};
+
+#[allow(unreachable_code)]
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("$ ");
+        io::stdout().flush().expect("failed to flush stdout");
+
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("failed to read stdin");
+        let command = line.trim();
+
+        match unsafe { unistd::fork() } {
+            Ok(unistd::ForkResult::Parent { child }) => {
+                wait::waitpid(child, None).expect("failed to waitpid");
+            }
+            Ok(unistd::ForkResult::Child) => {
+                let command_cstr = ffi::CString::new(command).expect("failed to CString::new");
+                unistd::execvp(&command_cstr, &[command_cstr.clone()]).expect("failed to execvp");
+            }
+            Err(e) => panic!("failed to fork: {e}"),
+        }
+    }
 }
